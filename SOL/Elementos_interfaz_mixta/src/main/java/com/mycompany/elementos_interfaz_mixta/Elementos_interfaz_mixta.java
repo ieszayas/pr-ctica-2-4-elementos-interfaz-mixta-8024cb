defbca2c2c00173810909daffce2654be271891a
Pablo <8024.cb@gmail.com>
2025-10-16 17:48:40 +0200

    update

---
+++ SOL/Elementos_interfaz_mixta/src/main/java/com/mycompany/elementos_interfaz_mixta/Elementos_interfaz_mixta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.elementos_interfaz_mixta;
+
+/**
+ *
+ * @author PC_Gaëlle
+ */
+public class Elementos_interfaz_mixta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
